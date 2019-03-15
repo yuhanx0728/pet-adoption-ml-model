60f49352fb0fff60372b889702153f9c132e92ee
肖宇含 <yuhanx@andrew.cmu.edu>
2019-03-15 01:42:49 -0400

    add training script

--- model.py
+++ model.py
@@ -1,81 +1,111 @@
+EPOCHS = 1000
+H = 100
+LR = 0.001
+
+data_train = CSVDataset('data/train.csv')
+data_train_loader = data.DataLoader(data_train, shuffle=True, batch_size=16)
+# data_test = CSVDataset('data/test.csv')
+# data_test_loader = data.DataLoader(data_test, shuffle=True, batch_size=1)
+
+import pandas as pd
+import hyperparameters as hyp
+import torch.utils.data as data
+import torch.nn as nn
 import torch
-import numpy as np
+from tqdm import tqdm
+import torch.optim as optim
+import matplotlib.pyplot as plt
 
-class NeuralNetwork(object):
+class CSVDataset(data.Dataset):
+    # exclude some fields that are non-numeric
+    def __init__(self, file_path):
+        df = pd.read_csv(file_path)
+        df = df.drop(['Name', 'RescuerID', 'PetID', 'Description'], axis=1)
+        self.data = torch.FloatTensor(df.values).cuda()
+        
+    """  
+    (  tensor([[    2,     3,   299,     0,     1,     1,     7,     0,     1,     1,
+                  2,     2,     2,     1,     1,   100, 41326,     0,     1]],   dtype=torch.int32)  ,
+       tensor([2], dtype=torch.int32)  )
+    """  
+    def __getitem__(self, index):
+        label = self.data[index][-1].long().cuda()
+        return (self.data[index][:-1], label)
+
+    def __len__(self):
+        return len(self.data)
+
+class Model(nn.Module):
     def __init__(self):
-        #parameters
-        self.inputSize = 2
-        self.outputSize = 1
-        self.hiddenSize = 3
+        super().__init__()
+        self.base_model = nn.Sequential(torch.nn.Linear(19, H),
+                                        torch.nn.ReLU(),
+                                        torch.nn.Linear(H, H))
+        self.classification_layer = nn.Sequential(torch.nn.ReLU(),
+                                                  torch.nn.Linear(H, 5))
 
-        #weights
-        self.W1 = np.random.randn(
-            self.inputSize,
-            self.hiddenSize)  # (3x2) weight matrix from input to hidden layer
-        self.W2 = np.random.randn(
-            self.hiddenSize,
-            self.outputSize)  # (3x1) weight matrix from hidden to output layer
+    def forward(self, input):
+        # input : 19 x 1 matrix
+        hidden = self.base_model.forward(input)
+        output = self.classification_layer(hidden)
+        # output : 5 adoption speeds
+        return output
 
-    def forward(self, X):
-        #forward propagation through our network
-        self.z = np.dot(
-            X,
-            self.W1)  # dot product of X (input) and first set of 3x2 weights
-        self.z2 = self.sigmoid(self.z)  # activation function
-        self.z3 = np.dot(
-            self.z2, self.W2
-        )  # dot product of hidden layer (z2) and second set of 3x1 weights
-        o = self.sigmoid(self.z3)  # final activation function
-        return o
+    def train(self):
+        self.base_model.train()
 
-    def sigmoid(self, s):
-        # activation function
-        return 1 / (1 + np.exp(-s))
+    def eval(self):
+        self.base_model.eval()
 
-    def sigmoidPrime(self, s):
-        #derivative of sigmoid
-        return s * (1 - s)
+model = Model().cuda()
 
-    def backward(self, X, y, o):
-        # backward propgate through the network
-        self.o_error = y - o  # error in output
-        self.o_delta = self.o_error * self.sigmoidPrime(
-            o)  # applying derivative of sigmoid to error
+for epoch in range(EPOCHS):  # trains the NN 1,000 times
+    print("Training...")
+    model.train()
+    optimizer = optim.Adam(model.parameters(), lr=LR, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+    loss = nn.CrossEntropyLoss()
+    loss = loss.cuda()
+    epoch = 0
+    store_epoch_loss = []
+    store_epoch_loss_val = []
+    store_epoch_acc_val = []
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(EPOCHS/2), int(0.75*EPOCHS)], gamma=0.1)
+    try:
+        for e in tqdm(range(EPOCHS)):
+            scheduler.step()
+            epoch = e + 1
+            epoch_loss = 0
+            store_batch_loss = []
+            for batch_num, (X, y) in enumerate(data_train_loader):
+                optimizer.zero_grad()
+                prediction = model.forward(X)
+                batch_loss = loss(prediction, y)
+                batch_loss.backward()
+                optimizer.step()
+                store_batch_loss.append(batch_loss.clone().cpu())
+                epoch_loss = torch.FloatTensor(store_batch_loss).mean()
+            store_epoch_loss.append(epoch_loss)
+            torch.save(model.state_dict(), "dcheckpoint_{}.pth".format(epoch))
+            plt.plot(store_epoch_loss[1:], label="Training Loss")
 
-        self.z2_error = self.o_delta.dot(
-            self.W2.T
-        )  # z2 error: how much our hidden layer weights contributed to output error
-        self.z2_delta = self.z2_error * self.sigmoidPrime(
-            self.z2)  # applying derivative of sigmoid to z2 error
+            model.eval()
 
-        self.W1 += X.T.dot(
-            self.z2_delta)  # adjusting first set (input --> hidden) weights
-        self.W2 += self.z2.T.dot(
-            self.o_delta)  # adjusting second set (hidden --> output) weights
-
-    def train(self, X, y):
-        o = self.forward(X)
-        self.backward(X, y, o)
-
-    def saveWeights(self):
-        np.savetxt("w1.txt", self.W1, fmt="%s")
-        np.savetxt("w2.txt", self.W2, fmt="%s")
-
-    def predict(self, data):
-        print("Predicted data based on trained weights: ")
-        print("Input (scaled): \n" + str(data))
-        print("Output: \n" + str(self.forward(data)))
-
-NN = NeuralNetwork()
-for i in range(hyp.EPOCH):  # trains the NN 1,000 times
-    print(" #" + str(i) + "\n")
-    print("Input (scaled): \n" + str(X))
-    print("Actual Output: \n" + str(y))
-    print("Predicted Output: \n" + str(NN.forward(X)))
-    print("Loss: \n" + str(np.mean(
-        np.square(y - NN.forward(X)))))  # mean sum squared loss
-    print("\n")
-    NN.train(X, y)
-
-NN.saveWeights()
-NN.predict()
+            plt.legend()
+            plt.grid()
+            plt.savefig("Loss.png".format())
+            plt.close()
+            model.train()
+        most_acc = max(store_epoch_acc_val)
+        min_loss = min(store_epoch_loss_val)
+        print("\nHighest accuracy of {} occured at {}%...Minimum loss occured at {}%...".format(most_acc, store_epoch_acc_val.index(most_acc)+1, store_epoch_loss_val.index(min_loss)+1))
+        t = Timer(3*60, interrupt_handler, ["checkpoint_{}.pth".format(store_epoch_loss_val.index(min_loss)+1), "{}/checkpoint_{}.pth".format(training_dir, store_epoch_acc_val.index(most_acc)+1)])
+        t.start()
+        user_pick = input("Which checkpoint do you want to use ?\n")
+        t.cancel()
+        model.load_state_dict(torch.load("checkpoint_{}.pth".format(user_pick)))
+    except KeyboardInterrupt:
+        most_acc = max(store_epoch_acc_val)
+        min_loss = min(store_epoch_loss_val)
+        print("\nHighest accuracy of {} occured at {}%...Minimum loss occured at {}%...".format(most_acc, store_epoch_acc_val.index(most_acc)+1, store_epoch_loss_val.index(min_loss)+1))
+        user_pick = input("Which checkpoint do you want to use ?\n")
+        model.load_state_dict(torch.load("checkpoint_{}.pth".format(user_pick)))
